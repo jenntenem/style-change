304313e401322e98833725999701caa6ec7c6aed
jenntenem <jenntenem@gmail.com>
2023-04-27 16:17:32 -0500

    y __init__ execute main function

---
+++ src/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""
+Import the dataset and save it in a csv and xlsx file. 
+After, load the dataset from the csv file and vectorize the texts.
+And finally, train the model and test it.
+"""
+
+
+def main():
+    pass
